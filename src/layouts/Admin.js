62b1815e017096a5630ba41596a910083ee00e94
easyblend <kenzieemma072@gmail.com>
2023-01-24 21:13:36 +0000

    understood the charts js

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -104,7 +104,7 @@ const Admin = (props) => {
           <Redirect from="*" to="/admin/index" />
         </Switch>
         <Container fluid>
-          <AdminFooter />
+          <AdminFooter /
         </Container>
       </div>
     </>
