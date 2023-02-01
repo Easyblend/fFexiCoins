feb70d346f37236b2de38460b4da9c6695d4fdd6
easyblend <kenzieemma072@gmail.com>
2023-02-01 08:20:58 +0000

    toast.error gave too many popups so i threw the errors insteading of logging them

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -18,7 +18,7 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, Route, Switch, Redirect } from "react-router-dom";
 // reactstrap components
-import { Button, Container } from "reactstrap";
+import { Container } from "reactstrap";
 // core components
 import AdminNavbar from "components/Navbars/AdminNavbar.js";
 import AdminFooter from "components/Footers/AdminFooter.js";
@@ -28,8 +28,6 @@ import routes from "routes.js";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "variables/FirebaseConfig";
 import { useNavigate } from "react-router-dom-v5-compat";
-import { database } from "variables/FirebaseConfig";
-import { collection, doc, getDoc, getDocs, setDoc } from "firebase/firestore";
 
 import Modal2 from "views/examples/Modal2";
 import Modal from "views/examples/Modal";
@@ -99,7 +97,9 @@ const Admin = (props) => {
       const data = await response.json();
       setDollarRate(data.new_amount);
     } catch (error) {
-      toast.info("Something's not right, refresh the page");
+      toast.info(
+        "Coundln't get current exchange rate, please check you connection"
+      );
     }
   };
 
@@ -119,20 +119,28 @@ const Admin = (props) => {
 
   //Getting BTC rate in dollars
   const getBtcRate = async () => {
-    const response = await fetch(
-      "https://api.coingecko.com/api/v3/coins/bitcoin"
-    );
-    const data = await response.json();
-    setBtcRate(data.market_data.current_price.usd); //btc price in dollars
+    try {
+      const response = await fetch(
+        "https://api.coingecko.com/api/v3/coins/bitcoin"
+      );
+      const data = await response.json();
+      setBtcRate(data.market_data.current_price.usd); //btc price in dollars
+    } catch (error) {
+      throw error;
+    }
   };
 
   //get Ethereum Rate in USD
   const getEthRate = async () => {
-    const response = await fetch(
-      "https://api.coingecko.com/api/v3/coins/ethereum"
-    );
-    const data = await response.json();
-    setEthRate(data.market_data.current_price.usd); //btc price in dollars
+    try {
+      const response = await fetch(
+        "https://api.coingecko.com/api/v3/coins/ethereum"
+      );
+      const data = await response.json();
+      setEthRate(data.market_data.current_price.usd); //btc price in dollars
+    } catch (error) {
+      throw error;
+    }
   };
 
   useEffect(() => getEthRate);
@@ -164,8 +172,6 @@ const Admin = (props) => {
 
   const [modal, setModal] = useState(false);
   const [modal2, setModal2] = useState(false);
-
-  const [datas, setDatas] = useState([]);
 
   const getTransactions = async () => {
     // try {
